1a5ff0e273e0cba9654f325f94e8f866905d8d42
Richard Cox <6799456+RickCoxDev@users.noreply.github.com>
2021-10-13 10:51:16 -0500

    Adding dotfiles and apps

---
+++ dotfiles/config.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set -x GPG_TTY (tty)
+set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+gpgconf --launch gpg-agent
+
+set -gx PATH $PATH $HOME/.krew/bin
+
+starship init fish | source
