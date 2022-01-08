a9d5f9a7fc5c4fcce1b11b7063a0a1ac1b6e881d
Richard Cox <6799456+RickCoxDev@users.noreply.github.com>
2022-01-07 23:52:17 -0600

    chore(fish): Removing greeting text in fish

--- dotfiles/config.fish
+++ dotfiles/config.fish
@@ -2,6 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+set fish_greeting
+
 set -x GPG_TTY (tty)
 set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
 gpgconf --launch gpg-agent
