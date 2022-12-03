9c78f93bb24e4b0b06a9709a00ab6f3b19beab29
Richard Cox <cox.richard200@gmail.com>
2022-12-02 23:00:57 -0600

    feat(powershell): Adding tab completion

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,7 @@
+$env:BW_SESSION=""
+
 Invoke-Expression (&starship init powershell)
-set BW_SESSION=""
+kubectl completion powershell | Out-String | Invoke-Expression
+flux completion powershell | Out-String | Invoke-Expression
+gh completion -s powershell | Out-String | Invoke-Expression
+starship completions powershell | Out-String | Invoke-Expression
