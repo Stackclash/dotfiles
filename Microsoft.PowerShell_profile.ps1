5712b5fa5651afd5f84cd1fdeb5574e840d51c4c
Richard Cox <cox.richard200@gmail.com>
2022-12-04 07:15:29 -0600

    chore: Adding comments to rc files

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,10 @@
+#### BITWARDEN SESSION ENVIRONMENT VARIABLE
 $env:BW_SESSION=""
 
+#### INITIALIZE STARSHIP
 Invoke-Expression (&starship init powershell)
+
+#### ADD TAB COMPLETIONS
 kubectl completion powershell | Out-String | Invoke-Expression
 flux completion powershell | Out-String | Invoke-Expression
 gh completion -s powershell | Out-String | Invoke-Expression
