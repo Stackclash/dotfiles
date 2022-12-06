d04dfc406d7a30411884f2fb84a635f3f8955e8e
Richard Cox <cox.richard200@gmail.com>
2022-12-06 14:36:11 -0600

    chore: adding chezmoi tab completion

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -5,9 +5,10 @@ $env:BW_SESSION=""
 Invoke-Expression (&starship init powershell)
 
 #### ADD TAB COMPLETIONS
-kubectl completion powershell | Out-String | Invoke-Expression
+chezmoi completion powershell | Out-String | Invoke-Expression
 flux completion powershell | Out-String | Invoke-Expression
 gh completion -s powershell | Out-String | Invoke-Expression
+kubectl completion powershell | Out-String | Invoke-Expression
 starship completions powershell | Out-String | Invoke-Expression
 
 #### START GPG-AGENT
