ef73a424788c0eaa9c5f531eeadc1712bfb64d3f
Richard Cox <cox.richard200@gmail.com>
2022-12-06 10:07:24 -0600

    Starting gpg-connect-agent on windows

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -9,3 +9,6 @@ kubectl completion powershell | Out-String | Invoke-Expression
 flux completion powershell | Out-String | Invoke-Expression
 gh completion -s powershell | Out-String | Invoke-Expression
 starship completions powershell | Out-String | Invoke-Expression
+
+#### START GPG-AGENT
+gpg-connect-agent /bye
