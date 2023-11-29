1d20f5c1009b639bf83f45818e6ccd5b4a892b1b
Richard Cox <cox.richard200@gmail.com>
2023-11-29 06:51:02 -0600

    chore: updating dotfiles

--- dot_cmdrc.cmd
+++ dot_cmdrc.cmd
@@ -3,6 +3,9 @@
 :::: BITWARDEN SESSION ENVIRONMENT VARIABLE
 set BW_SESSION=""
 
+:::: DISABLE CLINK WELCOME MESSAGE
+(clink autorun install -- --quiet) > nul 2>&1
+
 :::: START GPG-AGENT
 (gpg-connect-agent killagent /bye) > nul 2>&1
 (gpg-connect-agent /bye) > nul 2>&1
