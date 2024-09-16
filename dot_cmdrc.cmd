7baf8cf6e2f9a65cac2bb1ea827d3a59f9d07983
Richard Cox <cox.richard200@gmail.com>
2024-09-16 15:07:35 -0500

    fixing clink to be quiet when starting

--- dot_cmdrc.cmd
+++ dot_cmdrc.cmd
@@ -3,9 +3,6 @@
 :::: BITWARDEN SESSION ENVIRONMENT VARIABLE
 set BW_SESSION=""
 
-:::: DISABLE CLINK WELCOME MESSAGE
-(clink autorun install -- --quiet) > nul 2>&1
-
 :::: START GPG-AGENT
 (gpg-connect-agent killagent /bye) > nul 2>&1
 (gpg-connect-agent /bye) > nul 2>&1
