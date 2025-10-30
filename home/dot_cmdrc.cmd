b02179bca3de362e2ae1ccb3b3d67db44cc1c462
Richard Cox <rick.cox@ssmhealth.com>
2025-10-30 06:52:38 -0500

    cleaning up files after changes

--- home/dot_cmdrc.cmd
+++ home/dot_cmdrc.cmd
@@ -1,11 +1,4 @@
 @echo off
 
-:::: BITWARDEN SESSION ENVIRONMENT VARIABLE
-set BW_SESSION=""
-
-:::: START GPG-AGENT
-(gpg-connect-agent killagent /bye) > nul 2>&1
-(gpg-connect-agent /bye) > nul 2>&1
-
 :::: ACTIVATE MISE ENVIRONMENT
 mise activate
