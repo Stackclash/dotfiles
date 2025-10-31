490791734f358ad264b2d5a87a1da4b3e753b8aa
Richard Cox <cox.richard200@gmail.com>
2025-10-31 12:13:30 -0500

    setting up oh-my-posh

--- home/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ home/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -3,7 +3,7 @@ mise activate pwsh | Out-String | Invoke-Expression
 mise install
 
 #### INITIALIZE OH-MY-POSH
-oh-my-posh init pwsh | Invoke-Expression
+oh-my-posh init pwsh --config "tokyo" | Invoke-Expression
 
 #### ADD TAB COMPLETIONS
 chezmoi completion powershell | Out-String | Invoke-Expression
